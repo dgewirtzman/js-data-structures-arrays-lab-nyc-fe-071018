fbf2c611919bdd08ff70c0e46cea44aa2c91762f
David Gewirtzman <dgewirtzman@playbill.com>
2018-08-15 00:21:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function prependDriver(name) {
   return [name, ...drivers];
 }
 
-function removeLastDriver(drivers) {
+function removeLastDriver(newArray) {
   let newArray = [...drivers];
   return newArray.pop();
 }
